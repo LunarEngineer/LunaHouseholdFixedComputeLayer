e410e4940e9ed66bc135598605fb24d995597fb3
Timothy Luna <timothy.d.luna@gmail.com>
2023-10-18 10:19:54 +0000

    chore(output): solidify output

--- output.tf
+++ output.tf
@@ -5,8 +5,9 @@
 #####################
 
 output "master_ip" {
-  value = resource.proxmox_vm_qemu.k8s_master[var.k8s_nodes.k8s_nodes_master[0]].default_ipv4_address
+  value = { for vm in resource.proxmox_vm_qemu.k8s_master: vm.name => vm.default_ipv4_address }
 }
+
 
 output "control_plane_ips" {
   value = { for vm in resource.proxmox_vm_qemu.k8s_control: vm.name => vm.default_ipv4_address }
