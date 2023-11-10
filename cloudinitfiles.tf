0b39fabc8b7044a387cfe2b6205a94487e2664fa
Timothy Luna <timothy.d.luna@gmail.com>
2023-11-10 02:55:34 +0000

    chore(firststab): getting tests

--- cloudinitfiles.tf
+++ cloudinitfiles.tf
@@ -1,5 +1,21 @@
-# module "filesbaby" {
-#   source            = "git@github.com:lunarengineer-bot/lunar-engineering-cloud-init-kube.git"
-#   organization_name = "LunaHousehold"
-#   hosts             = module.luna_cloud.lookatmenodes
-# }
+module "cloudinit_files" {
+  source            = "git@github.com:lunarengineer-bot/lunar-engineering-cloud-init-kube.git"
+  organization_name = variable.organization_name
+  hosts             = data.proxmox_virtual_environment_nodes.available_nodes
+  users             = [{
+    username         = "tim",
+    sudo             = true,
+    ssh_public_key   = "Whatever"
+  }]
+  groups = [{
+    groupname    = "gamers",
+    groupmembers = ["tim"]
+  }]
+  packages = ["ntp", "qemu-guest-agent"]
+}
+
+output "cloudinit_files" {
+  description = "Cloud init files generated for the hosts"
+  sensitive = false
+  value = module.cloudinit_files.output.cloudinitfile
+}
